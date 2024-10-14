d550543c239c31cab4bc0fc5eb05908975375b03
akkiadepu <149579349+akkiadepu@users.noreply.github.com>
2024-10-14 09:43:33 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -13,7 +13,7 @@ function Header() {
     
   return (
     <div>
-         {/* <header>
+         <header>
         <div className="container1">
             <div className="logo1">
                 <img src={logo} alt="SN Repair Logo"/>
@@ -32,42 +32,42 @@ function Header() {
                 <p>Have any questions? <br/> <a href="#">+91 8790096926</a></p>
             </div>
         </div>
-    </header> */}
+    </header> 
 
 
   
-      <div className="container1">
-        <div className="logo1">
-          <img src={logo} alt="SN Repair Logo" />
-        </div>
-        <button className="mobile-menu-btn" onClick={toggleMenu}>
-          &#9776; {/* Mobile menu icon */}
-        </button>
-        <nav>
-          <ul className={menuOpen ? "menu show" : "menu"}>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/services">Services</NavLink>
-            </li>
-            <li>
-              <NavLink to="/activits">Activits</NavLink>
-            </li>
-            <li>
-              <NavLink to="/aboutUs">AboutUs</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contactUs">ContactUs</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div className="contact-info">
-          <p>
-            Have any questions? <br /> <a href="#">+91 8790096926</a>
-          </p>
-        </div>
-      </div>
+      // <div className="container1">
+      //   <div className="logo1">
+      //     <img src={logo} alt="SN Repair Logo" />
+      //   </div>
+      //   <button className="mobile-menu-btn" onClick={toggleMenu}>
+      //     &#9776; {/* Mobile menu icon */}
+      //   </button>
+      //   <nav>
+      //     <ul className={menuOpen ? "menu show" : "menu"}>
+      //       <li>
+      //         <NavLink to="/">Home</NavLink>
+      //       </li>
+      //       <li>
+      //         <NavLink to="/services">Services</NavLink>
+      //       </li>
+      //       <li>
+      //         <NavLink to="/activits">Activits</NavLink>
+      //       </li>
+      //       <li>
+      //         <NavLink to="/aboutUs">AboutUs</NavLink>
+      //       </li>
+      //       <li>
+      //         <NavLink to="/contactUs">ContactUs</NavLink>
+      //       </li>
+      //     </ul>
+      //   </nav>
+      //   <div className="contact-info">
+      //     <p>
+      //       Have any questions? <br /> <a href="#">+91 8790096926</a>
+      //     </p>
+      //   </div>
+      // </div>
     
 
     </div>
